3a93061c2de37d3c524d3adfb9ef3b717792ff88
asier <amontero023@ikasle.ehu.eus>
2024-03-07 10:01:42 +0100

    TestCreateTable sin acabar

---
+++ MiniSQLServer/src/test/java/eus/ehu/giigsi/abd/parser/TestCreateTable.java
@@ -0,0 +1,19 @@
+package eus.ehu.giigsi.abd.parser;
+
+import eus.ehu.giigsi.abd.structures.Column;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestCreateTable {
+
+    @Test
+    public void prueba1() {
+        List<Column> columns = new ArrayList<Column>();
+        columns.add(new Column(Column.DataType.STRING, "Str"));
+        columns.add(new Column(Column.DataType.DOUBLE, "Dbl"));
+        columns.add(new Column(Column.DataType.INT, "Int"));
+    }
+
+}
