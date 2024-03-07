a5c7e0db7e670b71efbb3e3f237f1eee98bfbcef
asier <amontero023@ikasle.ehu.eus>
2024-03-07 09:46:37 +0100

    Clase CreateTable terminada sin test
    Recuperamos los parametros de columnsParameters para crear una columna por cada par de elloos, y mediante el nombre de la tabla creamos la tabla
    
    Finalmente, mostramos por pantalla el nombre de la tabla y en cada línea el nombre de cada columna

--- MiniSQLServer/src/main/java/eus/ehu/giigsi/abd/parser/CreateTable.java
+++ MiniSQLServer/src/main/java/eus/ehu/giigsi/abd/parser/CreateTable.java
@@ -1,6 +1,8 @@
 package eus.ehu.giigsi.abd.parser;
 
+import eus.ehu.giigsi.abd.structures.Column;
 import eus.ehu.giigsi.abd.structures.Database;
+import eus.ehu.giigsi.abd.structures.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,6 +24,18 @@ public class CreateTable implements MiniSQLQuery{
     }
     public String execute(Database database)
     {
-        return null;
+        List<Column> listColumns = new ArrayList<Column>();
+
+        String result = "Table:" + Table + "\n";
+
+        for(ColumnParameters c : columnsParameters) {
+            Column column = new Column(c.getType(), c.getName());
+            listColumns.add(column);
+            result += column.getName() + "\n";
+        }
+
+        Table t = new Table(this.Table, listColumns);
+
+        return result;
     }
 }
