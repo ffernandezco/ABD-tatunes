a6837b717af0c86e4fec826baaddfe3bc163765e
asier <amontero023@ikasle.ehu.eus>
2024-03-11 10:44:04 +0100

    TestCreateTable sin terminar, a la espera de aclarar ciertas ideas

--- MiniSQLServer/src/test/java/eus/ehu/giigsi/abd/parser/TestCreateTable.java
+++ MiniSQLServer/src/test/java/eus/ehu/giigsi/abd/parser/TestCreateTable.java
@@ -1,6 +1,8 @@
 package eus.ehu.giigsi.abd.parser;
 
 import eus.ehu.giigsi.abd.structures.Column;
+import eus.ehu.giigsi.abd.structures.Database;
+import eus.ehu.giigsi.abd.structures.Table;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -10,12 +12,20 @@ public class TestCreateTable {
 
     @Test
     public void prueba1() {
+        String result, expected;
+
         List<Column> columns = new ArrayList<Column>();
         columns.add(new Column(Column.DataType.STRING, "Str"));
         columns.add(new Column(Column.DataType.DOUBLE, "Dbl"));
         columns.add(new Column(Column.DataType.INT, "Int"));
 
+        List<ColumnParameters> columnParameters = new ArrayList<ColumnParameters>();
+
+        Table tablePrueba1 = new Table("Prueba1", columns);
+        //CreateTable ct = new CreateTable(tablePrueba1, columnParameters);
+
         //Clase 11/03
+        //result = ct.execute(null);
     }
 
 }
