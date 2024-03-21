b3bab6d4f802d572ce588445498569c6bf90b567
Francisco <franciscofercon@icloud.com>
2024-03-21 09:57:41 +0100

    Merge con el resto de clases + Mejoras en test

--- MiniSQLServer/src/test/java/eus/ehu/giigsi/abd/parser/TestInsert.java
+++ MiniSQLServer/src/test/java/eus/ehu/giigsi/abd/parser/TestInsert.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/* public class TestInsert {
+public class TestInsert {
 
     @Test
     public void TestInsert (){
@@ -54,4 +54,3 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 }
-*/
