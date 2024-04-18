98d3e7f91f7a47002140d215a72aeb8d93f7336c
Francisco <franciscofercon@icloud.com>
2024-04-18 09:59:36 +0200

    Commit final 18-04: se añaden expresiones regulares y actuaciones para CREATE TABLE y para DROP TABLE.

--- MiniSQLServer/src/main/java/eus/ehu/giigsi/abd/parser/MiniSQLParser.java
+++ MiniSQLServer/src/main/java/eus/ehu/giigsi/abd/parser/MiniSQLParser.java
@@ -1,5 +1,7 @@
 package eus.ehu.giigsi.abd.parser;
 
+import eus.ehu.giigsi.abd.structures.Column;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -7,19 +9,47 @@ import java.util.regex.Pattern;
 
 public class MiniSQLParser {
     public static final Pattern CREATE_SECURITY_PROFILE_PATTERN = Pattern.compile("CREATE\\s+SECURITY\\s+PROFILE\\s+(?<nombreSecurityProfile>[a-zA-Z]+)");
+    public static final Pattern CREATE_TABLE_PATTERN = Pattern.compile("CREATE\\s+TABLE\\s+(?<table>[a-zA-Z]+)\\s*\\((?<columns>[^)]+)\\)");
+    public static final Pattern DROP_TABLE_PATTERN = Pattern.compile("DROP\\s+TABLE\\s+(?<table>[a-zA-Z]+)");
     public static final int CREATE_SECURITY_PROFILE_PATTERN_GROUP_COUNT = 2;
     //TODO todos los demás patrones
-    public static final Pattern CREATE_TABLE_PATTERN = Pattern.compile("CREATE\\s+TABLE\\s+(?<nombre>[a-zA-Z]+)\\s*\\((?<columnas>[^)]+)\\)");
     public static final int CREATE_TABLE_PATTERN_GROUP_NAME = 1;
     public static final int CREATE_TABLE_PATTERN_GROUP_COLUMNS = 2;
-
+    public static final int DROP_TABLE_PATTERN_GROUP_NAME = 1;
     public static MiniSQLQuery parse(String miniSQLQuery)
     {
         Matcher matcher;
 
-
         //REGULAR MINISQL QUERIES
         //TODO
+
+        //CreateTable
+        matcher = CREATE_TABLE_PATTERN.matcher(miniSQLQuery);
+        if (matcher.find()) {
+            String tableName = matcher.group(CREATE_TABLE_PATTERN_GROUP_NAME);
+            String columnsString = matcher.group(CREATE_TABLE_PATTERN_GROUP_COLUMNS);
+            List<ColumnParameters> columnsParameters = new ArrayList<>();
+            List<String> columnStrings = commaSeparatedNames(columnsString);
+            for (String column : columnStrings) {
+                String[] parts = column.trim().split("\\s+");
+                if (parts.length == 2) {
+                    String columnName = parts[0];
+                    String dataTypeName = parts[1];
+                    Column.DataType dataType = DataTypeUtils.fromMiniSQLName(dataTypeName); // Convierte al tipo de dato automáticamente, sustituye al switch
+                    columnsParameters.add(new ColumnParameters(columnName, dataType));
+                }
+            }
+            return new CreateTable(tableName, columnsParameters);
+        }
+
+        //DropTable
+        matcher = DROP_TABLE_PATTERN.matcher(miniSQLQuery);
+        if (matcher.find()) {
+            String tableName = matcher.group(DROP_TABLE_PATTERN_GROUP_NAME);
+            return new DropTable(tableName);
+        }
+
+
 
         //SECURITY QUERIES
 
@@ -39,7 +69,6 @@ public class MiniSQLParser {
 
         return null;
     }
-
 
     static List<String> commaSeparatedNames(String text)
     {
