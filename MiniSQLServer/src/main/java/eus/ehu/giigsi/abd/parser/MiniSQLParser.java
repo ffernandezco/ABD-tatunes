c2cc808c9810f735bc481a5ddbdb74a190048468
Francisco <franciscofercon@icloud.com>
2024-04-22 11:56:33 +0200

    Commit final 22-04: se añade compatibilidad en el Parser con instrucciones SELECT

--- MiniSQLServer/src/main/java/eus/ehu/giigsi/abd/parser/MiniSQLParser.java
+++ MiniSQLServer/src/main/java/eus/ehu/giigsi/abd/parser/MiniSQLParser.java
@@ -11,11 +11,15 @@ public class MiniSQLParser {
     public static final Pattern CREATE_SECURITY_PROFILE_PATTERN = Pattern.compile("CREATE\\s+SECURITY\\s+PROFILE\\s+(?<nombreSecurityProfile>[a-zA-Z]+)");
     public static final Pattern CREATE_TABLE_PATTERN = Pattern.compile("CREATE\\s+TABLE\\s+(?<table>[a-zA-Z]+)\\s*\\((?<columns>[^)]+)\\)");
     public static final Pattern DROP_TABLE_PATTERN = Pattern.compile("DROP\\s+TABLE\\s+(?<table>[a-zA-Z]+)");
+    public static final Pattern SELECT_PATTERN = Pattern.compile("SELECT\\s+(?<columns>[a-zA-Z,\\s]+)\\s+FROM\\s+(?<tableName>[a-zA-Z]+)\\s*(?:WHERE\\s+(?<condition>.+))?");
     public static final int CREATE_SECURITY_PROFILE_PATTERN_GROUP_COUNT = 2;
     //TODO todos los demás patrones
     public static final int CREATE_TABLE_PATTERN_GROUP_NAME = 1;
     public static final int CREATE_TABLE_PATTERN_GROUP_COLUMNS = 2;
     public static final int DROP_TABLE_PATTERN_GROUP_NAME = 1;
+    public static final int SELECT_PATTERN_GROUP_COLUMNS = 1;
+    public static final int SELECT_PATTERN_GROUP_TABLE = 2;
+    public static final int SELECT_PATTERN_GROUP_CONDITION = 3;
     public static MiniSQLQuery parse(String miniSQLQuery)
     {
         Matcher matcher;
@@ -47,6 +51,28 @@ public class MiniSQLParser {
         if (matcher.find()) {
             String tableName = matcher.group(DROP_TABLE_PATTERN_GROUP_NAME);
             return new DropTable(tableName);
+        }
+
+        //Select
+        matcher = SELECT_PATTERN.matcher(miniSQLQuery);
+        if (matcher.find()) {
+            String table = matcher.group(SELECT_PATTERN_GROUP_TABLE);
+            String columnsString = matcher.group(SELECT_PATTERN_GROUP_COLUMNS);
+            String conditionString = matcher.group(SELECT_PATTERN_GROUP_CONDITION);
+
+            List<String> columns = commaSeparatedNames(columnsString);
+            Condition condition = null; // Podría no tener condición según el enunciado
+            if (conditionString != null && !conditionString.isEmpty()) {
+                String[] conditionData = conditionString.trim().split("\\s+");
+                //Creamos la Condition a partir de los valores solo si se incluye en el WHERE
+                if (conditionData.length == 3) {
+                    String column = conditionData[0];
+                    String operator = conditionData[1];
+                    String literalValue = conditionData[2];
+                    condition = new Condition(column, operator, literalValue);
+                }
+            }
+            return new Select(table, columns, condition);
         }
 
 
