78ae1e4b491300b603fd93de8394c69055295232
asier <amontero023@ikasle.ehu.eus>
2024-05-09 20:39:18 +0200

    Commit inicial para tener develop actualizado

--- MiniSQLServer/src/test/java/eus/ehu/giigsi/abd/security/TestManager.java
+++ MiniSQLServer/src/test/java/eus/ehu/giigsi/abd/security/TestManager.java
@@ -1,12 +1,17 @@
 package eus.ehu.giigsi.abd.security;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestManager {
 
     public Manager manager;
+    public String username;
 
-    @BeforeEach
-    public void init () {
-    }
+
 }
