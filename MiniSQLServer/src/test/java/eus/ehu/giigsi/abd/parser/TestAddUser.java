4145c3b268ec73c02d61fbea3d4acaa42860fa56
mauribryan <verastegui.g.mauricio@gmail.com>
2024-05-09 08:36:59 +0200

    commit 09/05

---
+++ MiniSQLServer/src/test/java/eus/ehu/giigsi/abd/parser/TestAddUser.java
@@ -0,0 +1,49 @@
+package eus.ehu.giigsi.abd.parser;
+
+import eus.ehu.giigsi.abd.Constants;
+import eus.ehu.giigsi.abd.security.Manager;
+import eus.ehu.giigsi.abd.security.Profile;
+import eus.ehu.giigsi.abd.security.User;
+import eus.ehu.giigsi.abd.structures.Database;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestAddUser {
+    private Database db;
+    private AddUser au,au2,au3,au4;
+
+    @BeforeEach
+    public void init(){
+        db = new Database("user", "user");
+        Manager mg = new Manager("manager");
+        db.securityManager = mg;
+        au = new AddUser("testUser", "testPass", "Admin");
+        au2= new AddUser(null,"testPass","Admin");
+        au3= new AddUser("testUser2",null,"Admin");
+        au4= new AddUser("testUser2","testPass",null);
+    }
+    @Test
+    public void prueba1(){
+        Profile adminProfile = new Profile();
+        adminProfile.setName(Profile.AdminProfileName);
+        db.getSecurityManager().getProfiles().add(adminProfile);
+
+        String result = au.execute(db);
+
+        assertEquals(Constants.ADD_USER_SUCCESS, result);
+        assertEquals(1, adminProfile.getUsers().size());
+        assertEquals("testUser", adminProfile.getUsers().get(0).getUsername());
+    }
+    @Test
+    public void prueba2(){
+        String result = au2.execute(db);
+        assertEquals("Usuario no agregado correctamente", result);
+        result = au3.execute(db);
+        assertEquals("Usuario no agregado correctamente", result);
+        result = au4.execute(db);
+        assertEquals("Usuario no agregado correctamente", result);
+    }
+}
---
+++ MiniSQLServer/src/test/java/eus/ehu/giigsi/abd/parser/TestDeleteUser.java
@@ -0,0 +1,57 @@
+package eus.ehu.giigsi.abd.parser;
+
+import eus.ehu.giigsi.abd.Constants;
+import eus.ehu.giigsi.abd.security.Manager;
+import eus.ehu.giigsi.abd.security.Profile;
+import eus.ehu.giigsi.abd.security.User;
+import eus.ehu.giigsi.abd.structures.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestDeleteUser {
+    private Database db;
+    private DeleteUser du,du2,du3,du4;
+
+    @BeforeEach
+    public void init() {
+        db = new Database("user", "user");
+        Manager mg = new Manager("manager");
+        db.securityManager = mg;
+        du = new DeleteUser("user1");
+        du2 = new DeleteUser(null);
+        du3 = new DeleteUser("");
+        du4 = new DeleteUser("user10");
+        Profile adminProfile = new Profile();
+        adminProfile.setName("adminProfileName");
+        Profile pf2 = new Profile();
+        pf2.setName("pf2");
+        User us = new User("user1","user1Pass");
+        User us2 = new User("user2", "user2pass");
+        adminProfile.getUsers().add(us);
+        adminProfile.getUsers().add(us2);
+        db.getSecurityManager().getProfiles().add(adminProfile);
+        db.getSecurityManager().getProfiles().add(pf2);
+    }
+    @Test
+    public void prueba1(){
+        String result = du.execute(db);
+        assertEquals(Constants.DELETE_USER_SUCCESS,result);
+    }
+    @Test
+    public void prueba2(){
+        String result = du2.execute(db);
+        assertEquals("Nombre de usuario no válido", result);
+    }
+    @Test
+    public void prueba3(){
+        String result = du3.execute(db);
+        assertEquals("Nombre de usuario no válido", result);
+    }
+    @Test
+    public void prueba4(){
+        String result = du4.execute(db);
+        assertEquals(Constants.USER_DOES_NOT_EXIST_ERROR,result);
+    }
+}
