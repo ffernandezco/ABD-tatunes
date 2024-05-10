c15c3b890a76eedc527118e16c7e1cf4a7372ac5
Francisco <franciscofercon@icloud.com>
2024-05-10 21:22:41 +0200

    Se añaden tests para verificar el funcionamiento de Encryption

---
+++ MiniSQLServer/src/test/java/eus/ehu/giigsi/abd/structures/TestEncryption.java
@@ -0,0 +1,22 @@
+package eus.ehu.giigsi.abd.structures;
+
+import eus.ehu.giigsi.abd.security.Encryption;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class TestEncryption {
+
+    @Test
+    public void testEncrypt() {
+        String text = "FRan";
+        String encriptado = "IUdq";
+
+        String encryptedMessage = Encryption.encrypt(text);
+        assertNotNull(encryptedMessage);
+        assertEquals(encryptedMessage, encriptado);
+
+    }
+
+}
